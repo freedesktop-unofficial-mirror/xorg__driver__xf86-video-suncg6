30eb6d11ceb6229adb4473a87cecf02d0cf9b266
Adam Jackson <ajax@nwnk.net>
2005-07-11 02:30:00 +0000

    Prep for modular builds by adding guarded #include "config.h" everywhere.

--- src/cg6_driver.c
+++ src/cg6_driver.c
@@ -22,6 +22,10 @@
  */
 /* $XFree86: xc/programs/Xserver/hw/xfree86/drivers/suncg6/cg6_driver.c,v 1.7 2002/12/06 16:44:38 tsi Exp $ */
 
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
 #include "xf86.h"
 #include "xf86_OSproc.h"
 #include "xf86_ansic.h"
